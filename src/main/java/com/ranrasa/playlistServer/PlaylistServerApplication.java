5596d040f2206aca8c3346ee1ef9bb0d521d6494
Sandaruwan, Chamin <Chamin.Sandaruwan@Pearson.Com>
2018-10-07 00:40:54 +0530

    fix

--- src/main/java/com/ranrasa/playlistServer/PlaylistServerApplication.java
+++ src/main/java/com/ranrasa/playlistServer/PlaylistServerApplication.java
@@ -2,9 +2,16 @@ package com.ranrasa.playlistServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class PlaylistServerApplication {
+public class PlaylistServerApplication extends SpringBootServletInitializer {
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(PlaylistServerApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(PlaylistServerApplication.class, args);
