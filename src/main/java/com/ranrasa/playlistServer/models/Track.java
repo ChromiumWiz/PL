10d5bfcc2e09170b0ff4646a222f7b23a60a84dd
Sandaruwan, Chamin <Chamin.Sandaruwan@Pearson.Com>
2018-10-07 18:56:15 +0530

    table name

--- src/main/java/com/ranrasa/playlistServer/models/Track.java
+++ src/main/java/com/ranrasa/playlistServer/models/Track.java
@@ -5,8 +5,11 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+
 
 @Entity
+@Table(name = "Track")
 public class Track {
     
     @Id
